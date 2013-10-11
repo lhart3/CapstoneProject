d821375e1cffb1945ff7020293ba1fed021c39b1
Goodpasture <jgood5@uis.edu>
2013-10-10 19:26:33 -0500

    Created Card and Deck classes

--- CardGames/CardGames/Card.vb
+++ CardGames/CardGames/Card.vb
@@ -1,3 +1,72 @@
-﻿Public Class Card
+﻿Public Enum Suit
+    Diamonds
+    Spades
+    Clubs
+    Hearts
+End Enum
 
+Public Enum FaceValue
+    Two = 2
+    Three = 3
+    Four = 4
+    Five = 5
+    Six = 6
+    Seven = 7
+    Eight = 8
+    Nine = 9
+    Ten = 10
+    Jack = 11
+    Queen = 12
+    King = 13
+    Ace = 14
+End Enum
+
+Public Class Card
+    Private suitCard As Suit
+    Private faceValueCard As FaceValue
+    Private cardIsUp As Boolean
+    Private _suit As CardGames.Suit
+    Private _faceVal As FaceValue
+    Private _p3 As Boolean
+
+    Public Property Suit() As Suit
+        Get
+            Return suitCard
+        End Get
+        Set(ByVal value As Suit)
+            suitCard = value
+        End Set
+    End Property
+
+    Public Property FaceVal() As FaceValue
+        Get
+            Return faceValueCard
+        End Get
+        Set(ByVal value As FaceValue)
+            faceValueCard = value
+        End Set
+    End Property
+
+    Public Property IsCardUp() As Boolean
+        Get
+            Return cardIsUp
+        End Get
+        Set(ByVal value As Boolean)
+            cardIsUp = value
+        End Set
+    End Property
+    Public Sub New(ByVal suitCard As Suit, ByVal faceValueCard As FaceValue, ByVal cardIsUp As Boolean)
+        Me.suitCard = suitCard
+        Me.faceValueCard = faceValueCard
+        Me.cardIsUp = cardIsUp
+    End Sub
+
+    ''' <summary>
+    ''' Return the card as a string (i.e. "The Ace of Spades")
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Overloads Overrides Function ToString() As String
+        Return "The" & faceValueCard.ToString & "of" & suitCard.ToString
+    End Function
 End Class
--- CardGames/CardGames/Deck.vb
+++ CardGames/CardGames/Deck.vb
@@ -1,3 +1,41 @@
-﻿Public Class Deck
+﻿Imports CardGames.Card
+Public Class Deck
+    Protected cards As New List(Of Card)
 
+    Default Public ReadOnly Property _position(ByVal position As Integer) As Card
+        Get
+            Return cards(position)
+        End Get
+    End Property
+
+    Public Sub New()
+        For Each suit As Suit In System.Enum.GetValues(GetType(Suit))
+            For Each faceVal As FaceValue In System.Enum.GetValues(GetType(FaceValue))
+                cards.Add(New Card(suit, faceVal, True))
+            Next
+        Next
+    End Sub
+
+    Public Function Draw() As Card
+        Dim card As Card = cards(0)
+        cards.RemoveAt(0)
+        Return card
+    End Function
+
+    Public Sub Shuffle()
+        Dim random As New Random
+        Dim i As Integer = 0
+        While i < cards.Count
+            Dim index1 As Integer = i
+            Dim index2 As Integer = random.Next(cards.Count)
+            SwapCard(index1, index2)
+            i = i + 1
+        End While
+    End Sub
+
+    Private Sub SwapCard(ByVal index1 As Integer, ByVal index2 As Integer)
+        Dim card As Card = cards(index1)
+        cards(index1) = cards(index2)
+        cards(index2) = card
+    End Sub
 End Class
